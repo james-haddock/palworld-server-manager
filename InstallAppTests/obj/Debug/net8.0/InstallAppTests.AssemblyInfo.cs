ee27e564bea028f10bf7ca6a9b1eb4367168ba95
James Haddock <123553781+james-haddock@users.noreply.github.com>
2024-03-04 21:31:23 +0000

    ServerStatusCheckerTests

--- InstallAppTests/obj/Debug/net8.0/InstallAppTests.AssemblyInfo.cs
+++ InstallAppTests/obj/Debug/net8.0/InstallAppTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InstallAppTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f29437ba1fbbb8e2bd3cdc95f1c363943ca53c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f0e58d660ff5e809d9f463f121f806cea9c17a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("InstallAppTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InstallAppTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
